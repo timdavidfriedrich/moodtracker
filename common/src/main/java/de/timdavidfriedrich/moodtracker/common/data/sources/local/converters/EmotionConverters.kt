a68198e1501b6394d19708419e656e4eb039fda9
Tim David Friedrich <tim_david.friedrich@stud.htwk-leipzig.de>
2024-05-25 13:00:27 +0200

    [FIX] Expected BEGIN_OBJECT but was BEGIN_ARRAY

--- common/src/main/java/de/timdavidfriedrich/moodtracker/common/data/sources/local/converters/EmotionConverters.kt
+++ common/src/main/java/de/timdavidfriedrich/moodtracker/common/data/sources/local/converters/EmotionConverters.kt
@@ -3,17 +3,22 @@ package de.timdavidfriedrich.moodtracker.common.data.sources.local.converters
 import androidx.room.ProvidedTypeConverter
 import androidx.room.TypeConverter
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import de.timdavidfriedrich.moodtracker.common.data.sources.local.entities.EmotionEntity
+import java.lang.reflect.Type
+
 
 @ProvidedTypeConverter
 class EmotionConverters {
+    private val collectionType: Type = object : TypeToken<Collection<EmotionEntity>?>() {}.type
+
     @TypeConverter
     fun toEmotionEntity(value: String?): List<EmotionEntity>? {
-        return Gson().fromJson<List<EmotionEntity>>(value, EmotionEntity::class.java)
+        return Gson().fromJson(value, collectionType)
     }
 
     @TypeConverter
     fun fromEmotionEntity(emotion: List<EmotionEntity>?): String? {
-        return Gson().toJson(emotion)
+        return Gson().toJson(emotion, collectionType)
     }
 }
