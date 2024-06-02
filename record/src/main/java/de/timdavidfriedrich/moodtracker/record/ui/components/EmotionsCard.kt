8880ebf6248faa999daeb86198727449938a4cd6
Tim David Friedrich <tim_david.friedrich@stud.htwk-leipzig.de>
2024-06-02 21:44:38 +0200

    [CHANGE] emotion card item styling (line breaks etc)

--- record/src/main/java/de/timdavidfriedrich/moodtracker/record/ui/components/EmotionsCard.kt
+++ record/src/main/java/de/timdavidfriedrich/moodtracker/record/ui/components/EmotionsCard.kt
@@ -17,6 +17,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.Hyphens
+import androidx.compose.ui.text.style.LineBreak
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import de.timdavidfriedrich.moodtracker.common.domain.models.Emotion
 import de.timdavidfriedrich.moodtracker.common.domain.models.Record
@@ -74,7 +77,7 @@ private fun EmotionCardItem(
     Card(
         shape = MaterialTheme.shapes.small,
         onClick = { onAction(RecordAction.ToggleEmotion(emotion)) },
-        modifier = modifier.width(dimensionResource(commonR.dimen.grid_column_size_large)),
+        modifier = modifier.width(dimensionResource(commonR.dimen.grid_column_size_extra_large)),
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -82,9 +85,20 @@ private fun EmotionCardItem(
                 .fillMaxWidth()
                 .padding(dimensionResource(commonR.dimen.padding_extra_small))
         ) {
-            Text(emotion.icon, style = MaterialTheme.typography.titleLarge)
+            Text(
+                text = emotion.icon,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleLarge,
+            )
             Spacer(Modifier.height(dimensionResource(commonR.dimen.padding_extra_small)))
-            Text(emotion.name, style = MaterialTheme.typography.bodySmall)
+            Text(
+                text = emotion.name,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.bodySmall.copy(
+                    hyphens = Hyphens.Auto,
+                    lineBreak = LineBreak.Paragraph,
+                ),
+            )
         }
     }
 }
